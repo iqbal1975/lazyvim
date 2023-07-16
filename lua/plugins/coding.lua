4bae3adb24c852878900465cbff7973c1483c637
iqbal1975 <iqbal.thaker@gmail.com>
2023-07-16 16:23:50 +0530

    Update LunarVim config

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,52 +1,88 @@
 return {
-  "ChristianChiarulli/swenv.nvim",
-  -- Extend Auto Completion
+
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      { "hrsh7th/cmp-emoji" },
-      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+    "ChristianChiarulli/swenv.nvim",
+    -- Extend Auto Completion
+    {
+      "hrsh7th/nvim-cmp",
+      dependencies = {
+        { "hrsh7th/cmp-emoji" },
+        { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+        {
+          "Saecki/crates.nvim",
+          event = { "BufRead Cargo.toml" },
+          config = true,
+        },
+      },
+      ---@param opts cmp.ConfigSchema
+      opts = function(_, opts)
+        -- original LazyVim kind icon formatter
+        local format_kinds = opts.formatting.format
+        opts.formatting.format = function(entry, item)
+          format_kinds(entry, item) -- add icons
+          return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+        end
+        local cmp = require("cmp")
+        opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+          { name = "emoji" },
+          { name = "crates" },
+        }))
+        opts.mapping = cmp.mapping.preset.insert({
+          ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+          ["<C-e>"] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+          }),
+          -- Accept currently selected item. If none selected, `select` first item.
+          -- Set `select` to `false` to only confirm explicitly selected items.
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        })
+      end,
+    },
+    keys = {
       {
-        "Saecki/crates.nvim",
-        event = { "BufRead Cargo.toml" },
-        config = true,
+        "<leader>Y",
+        "<cmd>lua require('swenv.api').pick_venv()<cr>",
+        "Choose Env",
+        desc = "Python",
       },
     },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      -- original LazyVim kind icon formatter
-      local format_kinds = opts.formatting.format
-      opts.formatting.format = function(entry, item)
-        format_kinds(entry, item) -- add icons
-        return require("tailwindcss-colorizer-cmp").formatter(entry, item)
-      end
-      local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
-        { name = "emoji" },
-        { name = "crates" },
-      }))
-      opts.mapping = cmp.mapping.preset.insert({
-        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-e>"] = cmp.mapping({
-          i = cmp.mapping.abort(),
-          c = cmp.mapping.close(),
-        }),
-        -- Accept currently selected item. If none selected, `select` first item.
-        -- Set `select` to `false` to only confirm explicitly selected items.
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-      })
-    end,
   },
-  keys = {
-    {
-      "<leader>Y",
-      "<cmd>lua require('swenv.api').pick_venv()<cr>",
-      "Choose Env",
-      desc = "Python",
+
+  {
+    "p00f/clangd_extensions.nvim",
+    lazy = true,
+    config = function() end,
+    opts = {
+      extensions = {
+        inlay_hints = {
+          inline = false,
+        },
+        ast = {
+          --These require codicons (https://github.com/microsoft/vscode-codicons)
+          role_icons = {
+            type = "",
+            declaration = "",
+            expression = "",
+            specifier = "",
+            statement = "",
+            ["template argument"] = "",
+          },
+          kind_icons = {
+            Compound = "",
+            Recovery = "",
+            TranslationUnit = "",
+            PackExpansion = "",
+            TemplateTypeParm = "",
+            TemplateTemplateParm = "",
+            TemplateParamObject = "",
+          },
+        },
+      },
     },
   },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,7 +8,58 @@ return {
     { "jose-elias-alvarez/typescript.nvim" },
   },
   opts = {
-    servers = {},
+    servers = {
+      { eslint = {} },
+      {
+        clangd = {
+          keys = {
+            { "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+          },
+          root_dir = function(...)
+            -- using a root .clang-format or .clang-tidy file messes up projects, so remove them
+            return require("lspconfig.util").root_pattern(
+              "compile_commands.json",
+              "compile_flags.txt",
+              "configure.ac",
+              ".git"
+            )(...)
+          end,
+          capabilities = {
+            offsetEncoding = { "utf-16" },
+          },
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
+          },
+        },
+      },
+    },
+    setup = {
+      clangd = function(_, opts)
+        local clangd_ext_opts = require("lazyvim.util").opts("clangd_extensions.nvim")
+        require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+        return true
+      end,
+      eslint = function()
+        require("lazyvim.util").on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
+    },
   },
   setup = {},
 }
