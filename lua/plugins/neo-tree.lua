7b555d5faec3cffac1147094e1041d40aeb5fefb
iqbal1975 <iqbal.thaker@gmail.com>
2023-09-12 18:21:11 +0530

    Update LazyVim config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,73 +1,26 @@
 return {
   {
-    opts = function(_, opts)
-      opts.open_files_do_not_replace_types = opts.open_files_do_not_replace_types
-        or { "terminal", "Trouble", "qf", "Outline" }
-      table.insert(opts.open_files_do_not_replace_types, "edgy")
-    end,
+    opts = {
+      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+      open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+      window = {
+        mappings = {
+          ["<space>"] = "none",
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+      },
+    }
   },
 }
--- File explorer
--- {
---   "nvim-neo-tree/neo-tree.nvim",
---   branch = "v2.x",
---   module = "neo-tree",
---   cmd = "Neotree",
---   dependencies = { { "MunifTanjim/nui.nvim", module = "nui" }, "nvim-lua/plenary.nvim" },
---   opts = {
---     close_if_last_window = true,
---     enable_diagnostics = false,
---     source_selector = {
---       winbar = true,
---       content_layout = "center",
---     },
---     default_component_configs = {
---       indent = {
---         padding = 0,
---       },
---       icon = {
---         folder_closed = "",
---         folder_open = "",
---         folder_empty = "",
---       },
---       git_status = {
---         symbols = {
---           added = "",
---           deleted = "",
---           modified = "",
---           renamed = "",
---           untracked = "",
---           ignored = "",
---           unstaged = "",
---           staged = "",
---           conflict = "",
---         },
---       },
---     },
---     window = {
---       width = 30,
---       mappings = {
---         ["o"] = "open",
---         ["v"] = "open_vsplit",
---       },
---     },
---     filesystem = {
---       follow_current_file = true,
---       hijack_netrw_behavior = "open_current",
---       use_libuv_file_watcher = true,
---       filtered_items = {
---         visible = true,
---         hide_dotfiles = false,
---         hide_gitignored = true,
---       },
---     },
---     event_handlers = {
---       {
---         event = "neo_tree_buffer_enter",
---         handler = function(_)
---           vim.opt_local.signcolumn = "auto"
---         end,
---       },
---     },
---   },
--- },
