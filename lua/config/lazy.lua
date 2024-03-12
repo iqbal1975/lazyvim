90174f0b51550f9d2fc90a5a9fe66e2baf8b7e6e
iqbal1975 <iqbal.thaker@gmail.com>
2024-03-12 17:24:55 +0530

    Update LazyVim configuration

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
+    { import = "lazyvim.plugins.extras.editor.outline" },
     { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
