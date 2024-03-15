124865e124c47934c42277da7393d17759b6e72c
iqbal1975 <iqbal.thaker@gmail.com>
2024-03-15 23:03:59 +0530

    Update LazyVim configuration

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -27,7 +27,7 @@ return {
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
 
-    local luasnip = require("luasnip")
+    -- local luasnip = require("luasnip")
     local cmp = require("cmp")
 
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
@@ -73,8 +73,8 @@ return {
             cmp.select_next_item()
             -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
             -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
+            -- elseif luasnip.expand_or_jumpable() then
+            --   luasnip.expand_or_jump()
           elseif has_words_before() then
             cmp.complete()
           else
@@ -84,8 +84,8 @@ return {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
+          -- elseif luasnip.jumpable(-1) then
+          --   luasnip.jump(-1)
           else
             fallback()
           end
