837e51b16061b2fcffc7446d60b5cf60f4e6c508
iqbal1975 <iqbal.thaker@gmail.com>
2024-12-05 16:14:43 +0530

    Update LazyVim configuration

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,23 +2,6 @@ return {
   -- the opts function can also be used to change the default opts:
   {
     "nvim-lualine/lualine.nvim",
-    -- dependencies = "abeldekat/harpoonline",
-    -- config = function()
-    --   local Harpoonline = require("harpoonline")
-    --   Harpoonline.setup({
-    --     custom_formatter = Harpoonline.gen_override("extended", {
-    --       indicators = { "1", "2", "3", "4", "5" },
-    --       active_indicators = { "[1]", "[2]", "[3]", "[4]", "[5]" },
-    --     }),
-    --   })
-    --   local lualine_d = { Harpoonline.format, "filename" }
-    --
-    --   require("lualine").setup({
-    --     sections = {
-    --       lualine_d = lualine_d,
-    --     },
-    --   })
-    -- end,
     opts = function(_, opts)
       local trouble = require("trouble")
       local symbols = trouble.statusline({
@@ -32,7 +15,6 @@ return {
         symbols.get,
         cond = symbols.has,
       })
-      -- table.insert(opts.sections.lualine_x, "😄")
     end,
   },
 }
