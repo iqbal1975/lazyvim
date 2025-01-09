a05dff56db550878c0348949ba7fcde420a85d59
iqbal1975 <iqbal.thaker@gmail.com>
2025-01-09 19:06:53 +0530

    Update LazyVim configuration

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -9,173 +9,218 @@
 local trigger_text = ";"
 
 return {
-	"saghen/blink.cmp",
-	enabled = true,
-	opts = function(_, opts)
-		-- Merge custom sources with the existing ones from lazyvim
-		-- NOTE: by default lazyvim already includes the lazydev source, so not adding it here again
-		opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
-			default = { "lsp", "path", "snippets", "buffer", "copilot", "luasnip", "dadbod" },
-			providers = {
-				lsp = {
-					name = "lsp",
-					enabled = true,
-					module = "blink.cmp.sources.lsp",
-					kind = "LSP",
-					-- When linking markdown notes, I would get snippets and text in the
-					-- suggestions, I want those to show only if there are no LSP
-					-- suggestions
-					-- Disabling fallbacks as my snippets woudlnt show up
-					-- fallbacks = { "luasnip", "buffer" },
-					score_offset = 90, -- the higher the number, the higher the priority
-				},
-				luasnip = {
-					name = "luasnip",
-					enabled = true,
-					module = "blink.cmp.sources.luasnip",
-					min_keyword_length = 2,
-					fallbacks = { "snippets" },
-					score_offset = 85,
-					max_items = 8,
-					-- Only show luasnip items if I type the trigger_text characters, so
-					-- to expand the "bash" snippet, if the trigger_text is ";" I have to
-					-- type ";bash"
-					should_show_items = function()
-						local col = vim.api.nvim_win_get_cursor(0)[2]
-						local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
-						-- NOTE: remember that `trigger_text` is modified at the top of the file
-						return before_cursor:match(trigger_text .. "%w*$") ~= nil
-					end,
-					-- After accepting the completion, delete the trigger_text characters
-					-- from the final inserted text
-					transform_items = function(_, items)
-						local col = vim.api.nvim_win_get_cursor(0)[2]
-						local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
-						local trigger_pos = before_cursor:find(trigger_text .. "[^" .. trigger_text .. "]*$")
-						if trigger_pos then
-							for _, item in ipairs(items) do
-								item.textEdit = {
-									newText = item.insertText or item.label,
-									range = {
-										start = { line = vim.fn.line(".") - 1, character = trigger_pos - 1 },
-										["end"] = { line = vim.fn.line(".") - 1, character = col },
-									},
-								}
-							end
-						end
-						-- NOTE: After the transformation, I have to reload the luasnip source
-						-- Otherwise really crazy shit happens and I spent way too much time
-						-- figurig this out
-						vim.schedule(function()
-							require("blink.cmp").reload("luasnip")
-						end)
-						return items
-					end,
-				},
-				path = {
-					name = "Path",
-					module = "blink.cmp.sources.path",
-					score_offset = 3,
-					-- When typing a path, I would get snippets and text in the
-					-- suggestions, I want those to show only if there are no path
-					-- suggestions
-					fallbacks = { "luasnip", "buffer" },
-					opts = {
-						trailing_slash = false,
-						label_trailing_slash = true,
-						get_cwd = function(context)
-							return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
-						end,
-						show_hidden_files_by_default = true,
-					},
-				},
-				buffer = {
-					name = "Buffer",
-					enabled = true,
-					max_items = 3,
-					module = "blink.cmp.sources.buffer",
-					min_keyword_length = 4,
-				},
-				snippets = {
-					name = "snippets",
-					enabled = true,
-					max_items = 3,
-					module = "blink.cmp.sources.snippets",
-					min_keyword_length = 4,
-					score_offset = 80, -- the higher the number, the higher the priority
-				},
-				-- Example on how to configure dadbod found in the main repo
-				-- https://github.com/kristijanhusak/vim-dadbod-completion
-				dadbod = {
-					name = "Dadbod",
-					module = "vim_dadbod_completion.blink",
-					score_offset = 85, -- the higher the number, the higher the priority
-				},
-				-- Third class citizen mf always talking shit
-				copilot = {
-					name = "copilot",
-					enabled = true,
-					module = "blink-cmp-copilot",
-					kind = "Copilot",
-					min_keyword_length = 6,
-					score_offset = -100, -- the higher the number, the higher the priority
-					async = true,
-				},
-			},
-			-- command line completion, thanks to dpetka2001 in reddit
-			-- https://www.reddit.com/r/neovim/comments/1hjjf21/comment/m37fe4d/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-			cmdline = function()
-				local type = vim.fn.getcmdtype()
-				if type == "/" or type == "?" then
-					return { "buffer" }
-				end
-				if type == ":" then
-					return { "cmdline" }
-				end
-				return {}
-			end,
-		})
+  "saghen/blink.cmp",
+  enabled = true,
+  -- In case there are breaking changes and you want to go back to the last
+  -- working release
+  -- https://github.com/Saghen/blink.cmp/releases
+  -- version = "v0.9.3",
+  dependencies = {
+    "moyiz/blink-emoji.nvim",
+    "Kaiser-Yang/blink-cmp-dictionary",
+  },
+  opts = function(_, opts)
+    -- NOTE: The new way to enable LuaSnip
+    -- Merge custom sources with the existing ones from lazyvim
+    -- NOTE: by default lazyvim already includes the lazydev source, so not adding it here again
+    opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
+      default = { "lsp", "path", "snippets", "buffer", "copilot", "dadbod", "emoji", "dictionary" },
+      providers = {
+        lsp = {
+          name = "lsp",
+          enabled = true,
+          module = "blink.cmp.sources.lsp",
+          kind = "LSP",
+          -- When linking markdown notes, I would get snippets and text in the
+          -- suggestions, I want those to show only if there are no LSP
+          -- suggestions
+          -- Disabling fallbacks as my snippets wouldn't show up
+          -- Enabled fallbacks as this seems to be working now
+          fallbacks = { "snippets", "buffer" },
+          score_offset = 90, -- the higher the number, the higher the priority
+        },
+        path = {
+          name = "Path",
+          module = "blink.cmp.sources.path",
+          score_offset = 25,
+          -- When typing a path, I would get snippets and text in the
+          -- suggestions, I want those to show only if there are no path
+          -- suggestions
+          fallbacks = { "snippets", "buffer" },
+          opts = {
+            trailing_slash = false,
+            label_trailing_slash = true,
+            get_cwd = function(context)
+              return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+            end,
+            show_hidden_files_by_default = true,
+          },
+        },
+        buffer = {
+          name = "Buffer",
+          enabled = true,
+          max_items = 3,
+          module = "blink.cmp.sources.buffer",
+          min_keyword_length = 4,
+          score_offset = 15, -- the higher the number, the higher the priority
+        },
+        snippets = {
+          name = "snippets",
+          enabled = true,
+          max_items = 8,
+          min_keyword_length = 2,
+          module = "blink.cmp.sources.snippets",
+          score_offset = 85, -- the higher the number, the higher the priority
+          -- Only show snippets if I type the trigger_text characters, so
+          -- to expand the "bash" snippet, if the trigger_text is ";" I have to
+          -- type ";bash"
+          should_show_items = function()
+            local col = vim.api.nvim_win_get_cursor(0)[2]
+            local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
+            -- NOTE: remember that `trigger_text` is modified at the top of the file
+            return before_cursor:match(trigger_text .. "%w*$") ~= nil
+          end,
+          -- After accepting the completion, delete the trigger_text characters
+          -- from the final inserted text
+          transform_items = function(_, items)
+            local col = vim.api.nvim_win_get_cursor(0)[2]
+            local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
+            local trigger_pos = before_cursor:find(trigger_text .. "[^" .. trigger_text .. "]*$")
+            if trigger_pos then
+              for _, item in ipairs(items) do
+                item.textEdit = {
+                  newText = item.insertText or item.label,
+                  range = {
+                    start = { line = vim.fn.line(".") - 1, character = trigger_pos - 1 },
+                    ["end"] = { line = vim.fn.line(".") - 1, character = col },
+                  },
+                }
+              end
+            end
+            -- NOTE: After the transformation, I have to reload the luasnip source
+            -- Otherwise really crazy shit happens and I spent way too much time
+            -- figurig this out
+            vim.schedule(function()
+              require("blink.cmp").reload("snippets")
+            end)
+            return items
+          end,
+        },
+        -- Example on how to configure dadbod found in the main repo
+        -- https://github.com/kristijanhusak/vim-dadbod-completion
+        dadbod = {
+          name = "Dadbod",
+          module = "vim_dadbod_completion.blink",
+          score_offset = 85, -- the higher the number, the higher the priority
+        },
+        -- https://github.com/moyiz/blink-emoji.nvim
+        emoji = {
+          module = "blink-emoji",
+          name = "Emoji",
+          score_offset = 15, -- the higher the number, the higher the priority
+          opts = { insert = true }, -- Insert emoji (default) or complete its name
+        },
+        -- https://github.com/Kaiser-Yang/blink-cmp-dictionary
+        -- In macOS to get started with a dictionary:
+        -- cp /usr/share/dict/words ~/github/dotfiles-latest/dictionaries
+        dictionary = {
+          module = "blink-cmp-dictionary",
+          name = "Dict",
+          score_offset = 20, -- the higher the number, the higher the priority
+          enabled = true,
+          max_items = 8,
+          min_keyword_length = 3,
+          opts = {
+            get_command = {
+              "rg", -- make sure this command is available in your system
+              "--color=never",
+              "--no-line-number",
+              "--no-messages",
+              "--no-filename",
+              "--ignore-case",
+              "--",
+              "${prefix}", -- this will be replaced by the result of 'get_prefix' function
+              vim.fn.expand("~/github/dotfiles-latest/dictionaries/words"), -- where you dictionary is
+            },
+            documentation = {
+              enable = true, -- enable documentation to show the definition of the word
+              get_command = {
+                -- For the word definitions feature
+                -- make sure "wn" is available in your system
+                -- brew install wordnet
+                "wn",
+                "${word}", -- this will be replaced by the word to search
+                "-over",
+              },
+            },
+          },
+        },
+        -- Third class citizen mf always talking shit
+        copilot = {
+          name = "copilot",
+          enabled = true,
+          module = "blink-cmp-copilot",
+          kind = "Copilot",
+          min_keyword_length = 6,
+          score_offset = -100, -- the higher the number, the higher the priority
+          async = true,
+        },
+      },
+      -- command line completion, thanks to dpetka2001 in reddit
+      -- https://www.reddit.com/r/neovim/comments/1hjjf21/comment/m37fe4d/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+      cmdline = function()
+        local type = vim.fn.getcmdtype()
+        if type == "/" or type == "?" then
+          return { "buffer" }
+        end
+        if type == ":" then
+          return { "cmdline" }
+        end
+        return {}
+      end,
+    })
 
-		-- This comes from the luasnip extra, if you don't add it, won't be able to
-		-- jump forward or backward in luasnip snippets
-		-- https://www.lazyvim.org/extras/coding/luasnip#blinkcmp-optional
-		opts.snippets = {
-			expand = function(snippet)
-				require("luasnip").lsp_expand(snippet)
-			end,
-			active = function(filter)
-				if filter and filter.direction then
-					return require("luasnip").jumpable(filter.direction)
-				end
-				return require("luasnip").in_snippet()
-			end,
-			jump = function(direction)
-				require("luasnip").jump(direction)
-			end,
-		}
+    opts.snippets = {
+      preset = "luasnip",
+      -- This comes from the luasnip extra, if you don't add it, won't be able to
+      -- jump forward or backward in luasnip snippets
+      -- https://www.lazyvim.org/extras/coding/luasnip#blinkcmp-optional
+      expand = function(snippet)
+        require("luasnip").lsp_expand(snippet)
+      end,
+      active = function(filter)
+        if filter and filter.direction then
+          return require("luasnip").jumpable(filter.direction)
+        end
+        return require("luasnip").in_snippet()
+      end,
+      jump = function(direction)
+        require("luasnip").jump(direction)
+      end,
+    }
 
-		-- The default preset used by lazyvim accepts completions with enter
-		-- I don't like using enter because if on markdown and typing
-		-- something, but you want to go to the line below, if you press enter,
-		-- the completion will be accepted
-		-- https://cmp.saghen.dev/configuration/keymap.html#default
-		opts.keymap = {
-			preset = "default",
-			["<Tab>"] = { "snippet_forward", "fallback" },
-			["<S-Tab>"] = { "snippet_backward", "fallback" },
+    -- The default preset used by lazyvim accepts completions with enter
+    -- I don't like using enter because if on markdown and typing
+    -- something, but you want to go to the line below, if you press enter,
+    -- the completion will be accepted
+    -- https://cmp.saghen.dev/configuration/keymap.html#default
+    opts.keymap = {
+      preset = "default",
+      ["<Tab>"] = { "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
 
-			["<Up>"] = { "select_prev", "fallback" },
-			["<Down>"] = { "select_next", "fallback" },
-			["<C-p>"] = { "select_prev", "fallback" },
-			["<C-n>"] = { "select_next", "fallback" },
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback" },
+      ["<C-n>"] = { "select_next", "fallback" },
 
-			["<C-b>"] = { "scroll_documentation_up", "fallback" },
-			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+      ["<S-k>"] = { "scroll_documentation_up", "fallback" },
+      ["<S-j>"] = { "scroll_documentation_down", "fallback" },
 
-			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-			["<C-e>"] = { "hide", "fallback" },
-		}
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide", "fallback" },
+    }
 
-		return opts
-	end,
+    return opts
+  end,
 }
